37fc4360764c2f3a6574b26270f9f1f386f54766
romeo <rdumlao07@gmail.com>
2024-01-08 19:22:41 -0500

    created reactionSchema

---
+++ models/Reaction.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+//Reaction Schema
+const reactionSchema = new Schema ({
+    reactionId: {
+        type: Schema.Types.ObjectId,
+        default: () => new mongoose.Types.ObjectId()
+    },
+    reactionBody: { type: String, required: true, maxlength: 280 },
+    username: { type: String, required: true },
+    createdAt: { type: Date, default: Date.now }
+});
+
+module.exports = reactionSchema;
