159fa658a7de144beef16c2c25b1de4ea1c10de3
romeo <rdumlao07@gmail.com>
2024-01-08 21:41:17 -0500

    updated Thought file path

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const User = require('../models/User');
-const Thought = require('../models/Thoughts')
+const Thought = require('../models/Thought')
 const router = express.Router();
 
 //GET all users
