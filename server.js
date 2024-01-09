2b7be9e3f4245331d5e466096aa1046fde013ab6
romeo <rdumlao07@gmail.com>
2024-01-09 14:30:06 -0500

    update mongodb connection

--- server.js
+++ server.js
@@ -8,12 +8,7 @@ const app = express();
 app.use(express.json());
 
 //Connect to MongoDB
-mongoose.connect('mongodb://localhost/socialNetworkDB', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-})
-.then(() => console.log('Connected to MongoDB' ))
-.catch(err => console.error('Could not connect to MongoDB', err));
+mongoose.connect('mongodb://127.0.0.1:27017/socialNetworkDB')
 
 //Use routes with '/api' prefix
 app.use('/api/users', userRoutes);
