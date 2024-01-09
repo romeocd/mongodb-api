2a37a1ddb10033301e49eda696b41d7b10253031
romeo <rdumlao07@gmail.com>
2024-01-08 19:35:10 -0500

    userRoutes GET a single user by _id

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,7 +4,7 @@ const Thought = require('../models/Thoughts')
 const router = express.Router();
 
 //GET all users
-router.get('/', async (req,res) => {
+router.get('/', async (req, res) => {
     try {
         const users = await User.find({});
         res.json(users);
@@ -13,5 +13,16 @@ router.get('/', async (req,res) => {
     }
 });
 
-
+//Get a single user by _id
+router.get('/:id', async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id).populate('thoughts').populate('friends');
+        if (!user) {
+            return res.status(404).json({ message: 'User not found '});
+        }
+        res.json(user);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
 
