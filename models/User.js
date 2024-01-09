8ed44d7a7e167e7a9c0ed14abd72b51678f13de9
romeo <rdumlao07@gmail.com>
2024-01-08 19:07:50 -0500

    added user schema

---
+++ models/User.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+//User Schema
+const userSchema = new Schema({
+    username: { type: String, unique: true, required: true, trim: true },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        match: [/.+\@.+\..+/, 'Please fill a valid email address'] 
+    },
+    thoughts: [{ type: Schema.Types.ObjectId, ref: 'Thought' }],
+    friends: [{ type: Schema.Types.ObjectId, ref: 'User' }]
+});
