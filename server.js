83b0d1f1678b416645bf8ed2150945824213772a
romeo <rdumlao07@gmail.com>
2024-01-08 21:44:07 -0500

    updated port 3001

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use('*', (req, res) => {
 });
 
 //Start the server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
